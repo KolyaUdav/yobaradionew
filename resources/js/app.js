8533f6959cdee409543fe8fcab98566ab9948c44
Kolya Udav <sashamarunenko@gmail.com>
2023-08-19 19:28:45 +0300

    Установлен vue.js через vite, создан тестовый компонент

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
-import './bootstrap';
+import('./bootstrap');
+
+import { createApp } from 'vue/dist/vue.esm-bundler.js';
+import example from './components/example.vue';
+
+const app = createApp({
+    components: {
+        example,
+    }
+});
+
+app.mount('#app');
