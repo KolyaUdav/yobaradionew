9695cd2dbe1caf53a9774ec979d74885f39f6157
Kolya Udav <sashamarunenko@gmail.com>
2023-08-19 20:01:50 +0300

    Добавлен препроцессор sass, добавлены тестовые стили для тестового компонента vue.js

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,11 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel Vue</title>
-    @vite('resources/js/app.js');
+    @vite(['resources/js/app.js', 'resources/scss/app.scss', 'resources/css/app.css'])
 </head>
 <body>
 <div id="app">
-    <Example></Example>
+    <Example class="example"></Example>
 </div>
 </body>
 </html>
